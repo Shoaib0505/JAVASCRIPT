77c37e41814f602a9a0c39a6574c10b50850cdc9
Shoaib0505 <145893333+Shoaib0505@users.noreply.github.com>
2024-04-21 07:59:33 +0000

    deeply learn about Numbers and Math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 500
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.6578
+
+//console.log(otherNumber.toPrecision(3));
+
+const number = 1000000
+//console.log(number.toLocaleString('en-IN'));
+
+
+//++++++++++++ Maths +++++++++++++
+
+//console.log(Maths);
+// console.log(Math.abs(-4)); //it just convert negative value into positive
+// console.log(Math.round(5.4)); 
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.0));
+// console.log(Math.min(2,4,6,8));
+// console.log(Math.max(2,4,6,8));
+
+console.log(Math.random());// value always occure in 0. something
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min +1)) + min);
