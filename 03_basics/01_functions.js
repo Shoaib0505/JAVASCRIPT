f8a0a14d23c3bd4422361abeda80dd72573c69ae
Shoaib0505 <145893333+Shoaib0505@users.noreply.github.com>
2024-04-23 06:36:03 +0000

    deeply

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,4 +28,37 @@ function loginUserMessage (username = "khan"){
 }
 
 // console.log(loginUserMessage("Shoaib"))
-console.log(loginUserMessage());   //output : undefined
+//console.log(loginUserMessage());   //output : undefined
+
+
+function calculateCartPrice(...num1){
+          return num1
+}
+//console.log(calculateCartPrice(200,  300, 400, 234,234,3456,765,));//we use rest operator(...)
+
+const user = {
+  username : "Shoaib",
+  price  : 500
+}
+
+function handleObj(anyobject){
+     console.log(`User name is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//  handleObj(user);
+
+//there is another way to pass an obj this mathode is called direct object
+
+// handleObj({
+//   username : "Khan",
+//   price: 399
+// })
+
+
+// For arrays
+
+const newArray = [200,400,500,600]
+
+function returnSecondValue(getArray){
+  return getArray[2]
+}
+console.log(returnSecondValue(newArray));
